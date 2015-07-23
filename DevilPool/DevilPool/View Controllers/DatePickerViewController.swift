bf57e907c9387e46182b249fcd966123376728c9
Calvin Chueh <chc24@duke.edu>
2015-07-22 17:01:10 -0700

    Updated RDSFDatePicker to latest Version, Upload/Download from Parse to update Calendar.

--- DevilPool/DevilPool/View Controllers/DatePickerViewController.swift
+++ DevilPool/DevilPool/View Controllers/DatePickerViewController.swift
@@ -7,57 +7,93 @@
 //
 
 import UIKit
+import Parse
 
 protocol DatePickerDelegate {
     func didSelectDate(date: NSDate!)
 }
 
 class DatePickerViewController: UIViewController, RSDFDatePickerViewDelegate, RSDFDatePickerViewDataSource {
-
+    
     var delegate : DatePickerDelegate!
+    var datePosts : NSMutableSet = NSMutableSet()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        var query = PFQuery(className: "Post")
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        query.findObjectsInBackgroundWithBlock { (dates: [AnyObject]?, error: NSError?) -> Void in
+            
+            if let dates = dates as? [PFObject] {
+                for item in dates {
+                    if let date = item["fromDate"] as? NSDate {
+                        println(date)
+                        self.datePosts.addObject(dateFormatter.stringFromDate(date))
+                    }
+                }
+            }
+            
+            println("khkhkhkh")
+            
         var datePickerView: RSDFDatePickerView = RSDFDatePickerView(frame: self.view.bounds)
         datePickerView.delegate = self
         datePickerView.dataSource = self
+        
         self.view.addSubview(datePickerView)
+
+        }
+        
         
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
     
     // MARK: RSDFDatePickerViewDelegate
     
+    //Query Parse for Dates~
+    
+    func datePickerView(view: RSDFDatePickerView!, shouldMarkDate date: NSDate!) -> Bool {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        return datePosts.containsObject(dateFormatter.stringFromDate(date))
+    }
+    
+    func datePickerView(view: RSDFDatePickerView!, markImageColorForDate date: NSDate!) -> UIColor! {
+        return UIColor.greenColor()
+    }
+    
     func datePickerView(view: RSDFDatePickerView!, didSelectDate date: NSDate!) {
         println(date.description)
-        self.dismissViewControllerAnimated(true, completion: { () -> Void in
-            if let delegate = self.delegate {
-                delegate.didSelectDate(date)
-            }
-        })
+        self.navigationController?.popViewControllerAnimated(true)
+        if let delegate = self.delegate {
+            delegate.didSelectDate(date)
+        }
+       
     }
     
     func datePickerViewMarkedDates(view: RSDFDatePickerView!) -> [NSObject : AnyObject]! {
         return [NSObject() : ""]
     }
-
     
+        
 }
